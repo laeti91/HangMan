2741ebfec10c7f0f02eab02fdcd37a6be60c66ff
Juliette Campello <campello.juliette@gmail.com>
2024-09-30 08:47:32 +0200

    partie 1

--- hangman.go
+++ hangman.go
@@ -1,18 +1,50 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"math/rand"
 	"os"
 )
 
 func main() {
-	fmt.Println(MotATrouver())
+	fmt.Println(WordToFind())
 }
-func MotATrouver() string {
-	file, err := os.ReadFile("word.txt")
+
+func WordToFind() string {
+	f, err := os.Open("word.txt")
+	scanner := bufio.NewScanner(f)
+	nbrMots := 0
+
+	for scanner.Scan() {
+		nbrMots++
+	}
 
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 	}
-	fmt.Println(string(file))
+
+	randomNumber := rand.Intn(nbrMots)
+
+	return Scan(randomNumber)
+}
+
+func Scan(nbr int) string {
+	mot := ""
+	nbrMots2 := 0
+	f, err := os.Open("word.txt")
+	scanner := bufio.NewScanner(f)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for scanner.Scan() {
+		nbrMots2++
+		if nbrMots2 == nbr {
+			mot = scanner.Text()
+		}
+	}
+	return mot
 }
