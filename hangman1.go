af377fb2b634a5b35af8612ef0e9e83454c3973f
julian <julianterencesmith@gmail.com>
2024-09-30 10:14:28 +0200

    return random letters of a word with map

--- hangman1.go
+++ hangman1.go
@@ -6,10 +6,13 @@ import (
 	"log"
 	"math/rand"
 	"os"
+	"time"
 )
 
 func main() {
-	fmt.Println(WordToFind())
+	word := WordToFind()
+	fmt.Println(word)
+	fmt.Println(nUniqueRandomLetters(word))
 }
 
 func WordToFind() string {
@@ -49,3 +52,27 @@ func Scan(nbr int) string {
 	return mot
 }
 
+func nUniqueRandomLetters(word string) string {
+	n := len(word)/2 - 1
+
+	rand.Seed(time.Now().UnixNano())
+	letters := []rune(word)
+	rand.Shuffle(len(letters), func(i, j int) {
+		letters[i], letters[j] = letters[j], letters[i]
+	})
+
+	uniqueLetters := make(map[rune]bool)
+	result := make([]rune, 0, n)
+
+	for _, letter := range letters {
+		if !uniqueLetters[letter] {
+			uniqueLetters[letter] = true
+			result = append(result, letter)
+			if len(result) == n {
+				break
+			}
+		}
+	}
+
+	return string(result)
+}
