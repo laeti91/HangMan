d7a493895401ee97f1c21493472a2486dadd5595
julian <julianterencesmith@gmail.com>
2024-09-30 09:18:56 +0200

    making sure the program works

--- hangman.go
+++ hangman.go
@@ -1,18 +1,57 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"math/rand"
 	"os"
 )
 
 func main() {
-	fmt.Println(MotATrouver())
+	nbr := rand.Intn(chooseWordRandom())
+	fmt.Println(scanFile(nbr))
 }
-func MotATrouver() string {
-	file, err := os.ReadFile("word.txt")
+
+func chooseWordRandom() int {
+	f, err := os.Open("words2.txt")
 
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 	}
-	fmt.Println(string(file))
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	nbrMots := 0
+
+	for scanner.Scan() {
+		nbrMots++
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	return nbrMots
+}
+
+func scanFile(nbr int) string {
+	f, err := os.Open("words2.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	nbrMots := 0
+	mot := ""
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		nbrMots++
+		if nbrMots == nbr {
+			mot = scanner.Text()
+		}
+	}
+	return mot
 }
