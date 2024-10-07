ecde64421df9bf5e4c29684ae7552a61b41755f5
julian <julianterencesmith@gmail.com>
2024-10-07 12:23:47 +0200

    second not working

--- SecondHangman/hangman.go
+++ SecondHangman/hangman.go
@@ -1,5 +1,40 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
 
+func main() {
+	nbr := rune(')')
+	GetAsciiLett(int(nbr))
+}
+
+func GetAsciiLett(nbr int) {
+	f, err := os.Open("standard.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	ligne := -9
+
+	for scanner.Scan() {
+		ligne = ligne + 9
+		if ligne == (nbr-32)*9 {
+			for i := ligne; i < ligne+9; i++ {
+				print(i)
+				fmt.Println(scanner.Text())
+			}
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 }
